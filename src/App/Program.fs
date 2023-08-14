d96be03e44e47d82c30934375231c23b9e290b95
Adam Hodgson <adam.c.hodgson@gmail.com>
2023-08-14 13:46:51 +0100

    Simple Enigma Implementation
    Implementation is incomplete. Future work needed to advance rotors with each keystroke.

---
+++ src/App/Program.fs
@@ -0,0 +1,17 @@
+﻿open Enigma
+open System
+
+[<EntryPoint>]
+let main args =
+    printfn "Enigma Machine CLI"
+    printfn "Enter a message to encrypt:"
+    let input = Console.ReadLine()
+
+    let encryptedMessage =
+        input.ToUpper()
+        |> Seq.map (fun char -> if Char.IsLetter char then encryptChar enigmaConfig char else char)
+        |> String.Concat
+
+    printfn "Encrypted message: %s" encryptedMessage
+
+    0
---
+++ src/Library/Library.fs
@@ -0,0 +1,39 @@
+﻿module Enigma
+
+type Rotor = { Name: string; Wiring: string }
+
+let rotorI = { Name = "I"; Wiring = "EKMFLGDQVZNTOWYHXUSPAIBRCJ" }
+let rotorII = { Name = "II"; Wiring = "AJDKSIRUXBLHWTMCQGZNPYFVOE" }
+let rotorIII = { Name = "III"; Wiring = "BDFHJLCPRTXVZNYEIWGAKMUSQO" }
+
+// Enigma machine configuration
+type EnigmaMachine = { Rotors: Rotor list; Reflector: string; Plugboard: (char * char) list }
+
+let enigmaConfig = { Rotors = [rotorI; rotorII; rotorIII]; Reflector = "YRUHQSLDPXNGOKMIEBFZCWVJAT"; Plugboard = [('A', 'E'); ('G', 'Q'); ('P', 'V')] }
+
+// Perform rotor substitution
+let substitute rotor char =
+    let index = int (char - 'A')
+    rotor.Wiring.[index]
+
+// Perform rotor reverse substitution
+let reverseSubstitute rotor (letter: char) =
+    let index = rotor.Wiring.IndexOf(letter)
+    index + int 'A' |> char
+
+// Perform plugboard substitution
+let plugboardSubstitute plugboard char =
+    match List.tryFind (fun (a, b) -> a = char || b = char) plugboard with
+    | Some (a, b) -> if a = char then b else a
+    | None -> char
+
+// Simulate the Enigma machine encryption
+let encryptChar enigma char =
+    let substitutedChar = plugboardSubstitute enigma.Plugboard char
+    let rotorOutput =
+        enigma.Rotors
+        |> List.fold (fun char rotor -> substitute rotor char) substitutedChar
+        |> substitute { rotorIII with Wiring = enigma.Reflector }
+        |> List.foldBack (fun rotor char -> reverseSubstitute rotor char) enigma.Rotors
+
+    plugboardSubstitute enigma.Plugboard rotorOutput
